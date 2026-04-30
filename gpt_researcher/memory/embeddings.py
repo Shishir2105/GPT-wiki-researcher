93856ae9e9b6c99eb909601fedc361224674d0b2
Shishir2105 <shishir.sreeramadasu21@gmail.com>
2026-04-30 11:19:08 +0530

    Initial commit after changes

---
+++ gpt_researcher/memory/embeddings.py
@@ -0,0 +1,35 @@
+from langchain_community.vectorstores import FAISS
+import os
+
+
+class Memory:
+    def __init__(self, embedding_provider, **kwargs):
+
+        _embeddings = None
+        match embedding_provider:
+            case "ollama":
+                from langchain.embeddings import OllamaEmbeddings
+                _embeddings = OllamaEmbeddings(model="llama2")
+            case "openai":
+                from langchain_openai import OpenAIEmbeddings
+                _embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+            case "azureopenai":
+                from langchain_openai import AzureOpenAIEmbeddings
+                _embeddings = AzureOpenAIEmbeddings(deployment=os.environ["AZURE_EMBEDDING_MODEL"], chunk_size=16)
+            case "huggingface":
+                from langchain.embeddings import HuggingFaceEmbeddings
+                _embeddings = HuggingFaceEmbeddings()
+            
+            case "local":
+                from langchain_community.embeddings import HuggingFaceEmbeddings
+                self.embeddings = HuggingFaceEmbeddings(
+                model_name="sentence-transformers/all-MiniLM-L6-v2"
+                )
+
+            case _:
+                raise Exception("Embedding provider not found.")
+
+        self._embeddings = _embeddings
+
+    def get_embeddings(self):
+        return self._embeddings
